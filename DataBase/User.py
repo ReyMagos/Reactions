be660b54f87b12050567cf097c5c95cc131d20d0
Yarilo <yrikkonovalov91@gmail.com>
2022-04-09 15:24:59 +0500

    second

--- DataBase/User.py
+++ DataBase/User.py
@@ -2,6 +2,7 @@ import datetime
 import sqlalchemy
 from sqlalchemy import orm
 from .db_session import SqlAlchemyBase
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
 class User(SqlAlchemyBase):
@@ -14,3 +15,9 @@ class User(SqlAlchemyBase):
     about = sqlalchemy.Column(sqlalchemy.String, nullable=True)
     hashed_password = sqlalchemy.Column(sqlalchemy.String, nullable=True)
     review = orm.relation("Review", back_populates='user')
+
+    def set_password(self, password):
+        self.hashed_password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.hashed_password, password)
--- main.py
+++ main.py
@@ -1,6 +1,52 @@
 from flask import Flask
+from flask_login import LoginManager, login_required
+from werkzeug.utils import redirect
+from DataBase.User import User
+from DataBase import db_session
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'review14354fdggkdfgk'
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+
+@app.route("/")
+def index():
+    return "sfg"
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login_page():
+    return "login"
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register_page():
+    return "register"
+
+
+@app.route("/<int::id>", methods=["GET", "POST"])
+def page(user_id):
+    return "page"
+
+
+@app.route("/reviews/<name>", methods=["GET", "POST"])
+def cinema(name):
+    return f'{name}'
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    # logout_user()
+    return redirect("/")
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    db_sess = db_session.create_session()
+    return db_sess.query(User).get(user_id)
+
+
 if __name__ == "__main__":
     app.run()
