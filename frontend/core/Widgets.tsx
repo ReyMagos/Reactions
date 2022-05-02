29f6dc958a5acea4782da9711e95dfafa067c71e
ReyMagos <reymagos838@gmail.com>
2022-05-02 15:44:50 +0500

    Film search testing

--- frontend/core/Widgets.tsx
+++ frontend/core/Widgets.tsx
@@ -86,7 +86,7 @@ export const TextInput = (props: PTextInput) => {
       setDock(false)
     },
     onChange: (event: React.ChangeEvent<HTMLInputElement>) => {
-      axios.get("/films", { data: {text: event.target.value }})
+      axios.post("/films", { text: event.target.value })
         .then(response => dropdown.current.update(response.data.films.map(film => <p>{film}</p>)))
         .catch(error => console.log(error))
     }
